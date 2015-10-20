db41d681ca70442dff65a53307ae92ba0fbb8e1f
Liz Stevenson <lizstevenson4@gmail.com>
2015-10-20 15:29:15 +0100

    Manufacturing_no required added to search form

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,9 +2,8 @@ class Search < ActiveRecord::Base
 
 	def search_boats 
 		boats = Boat.all
-		boats = boats.where(["make LIKE ?", make]) if manufacturing_no.present?		
-		boats = boats.where(["manufacturing_no LIKE ?", manufacturing_no]) if manufacturing_no.present?
-
+		boats = boats.where(["make LIKE ? and manufacturing_no LIKE ?", make, manufacturing_no]) if manufacturing_no.present?		
+		
 		return boats
 	end
 end
